5f4170db4718733e1b5fe2099449789c28f27c5d
Christian Guizar <c-guizar@hotmail.com>
2021-09-05 18:49:46 -0700

    feature(selector): initialization

--- ping.js
+++ ping.js
@@ -2,18 +2,26 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
-const resetScore = document.querySelector('#reset')
+const resetScore = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3
+
+winningScoreSelect.addEventListener('change', () => {
+  winningScore = parseInt(winningScoreSelect.value);
+  resetto();
+});
 
 p1Button.addEventListener('click', () => {
-  if(p1Score !== winningScore && p2Score !== winningScore) {
+  if (p1Score !== winningScore && p2Score !== winningScore) {
     p1Score += 1;
     p1Display.textContent = p1Score;
+    colorChange();
   } else if (p1Score === winningScore || p2Score === winningScore) {
     p1Score += 0;
+    colorChange();
   }
 })
 
@@ -21,14 +29,28 @@ p2Button.addEventListener('click', () => {
   if(p2Score !== winningScore && p1Score !== winningScore) {
     p2Score += 1;
     p2Display.textContent = p2Score;
+    colorChange();
   } else if (p1Score === winningScore || p2Score === winningScore) {
     p2Score += 0;
+    colorChange();
   }
 })
 
-reset.addEventListener('click', () => {
+reset.addEventListener('click', resetto);
+
+function resetto() {
   p1Score = 0;
   p1Display.textContent = p1Score;
   p2Score = 0;
   p2Display.textContent = p2Score;
-})
+}
+
+function colorChange() {
+  if (p1Score === winningScore - 1) {
+    p1Display.classList.add('winner');
+    p2Display.classList.add('loser');
+  } else if (p2Score === winningScore - 1) {
+    p1Display.classList.add('loser');
+    p2Display.classList.add('winner');
+  }
+}
